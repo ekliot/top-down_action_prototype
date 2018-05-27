4be454ef9ea16740407937da5a1d264d638fee0a
ekliot <ekliot@gmail.com>
2018-05-27 16:12:04 -0500

    update state for new input mapping

--- characters/player/states/motion.gd
+++ characters/player/states/motion.gd
@@ -1,10 +1,10 @@
 extends "res://util/states/state.gd"
 
 func move_dir():
-  var u = Input.is_action_pressed( "ui_up" )
-  var d = Input.is_action_pressed( "ui_down" )
-  var l = Input.is_action_pressed( "ui_left" )
-  var r = Input.is_action_pressed( "ui_right" )
+  var u = Input.is_action_pressed( "move_up" )
+  var d = Input.is_action_pressed( "move_down" )
+  var l = Input.is_action_pressed( "move_left" )
+  var r = Input.is_action_pressed( "move_right" )
 
   var dir = Vector2( int(r) - int(l), int(d) - int(u) )
 
